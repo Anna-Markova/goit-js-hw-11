5111a666a8a428497251bad61421253199dd83c9
Anna-Markova <yavdokimenkoanna88@gmail.com>
2022-11-25 14:50:14 +0200

    images

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export async function fetchImages(inputValue,pageNr) {
+export async function fetchImages(trimmedValue, pageNumber) {
 
 const dataResponse = await axios.get
 (`https://pixabay.com/api/?key=29588079-fbc492831fdad231bf7222b96&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
